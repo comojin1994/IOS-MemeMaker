2d50d5c3604a3da353ad5ac82036256af22ef2f8
comojin1994 <comojin1994@gmail.com>
2021-01-01 17:25:22 +0900

    Refactor segmented controll function

--- memeMaker/memeMaker/ViewController.swift
+++ memeMaker/memeMaker/ViewController.swift
@@ -1,18 +1,11 @@
-//
-//  ViewController.swift
-//  memeMaker
-//
-//  Created by SungJin Kim on 2020/12/29.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var segmentTop: UISegmentedControl!
-    @IBOutlet weak var segmentBottom: UISegmentedControl!
+    @IBOutlet weak var topSegment: UISegmentedControl!
+    @IBOutlet weak var bottomSegment: UISegmentedControl!
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
-    @IBOutlet weak var puppyImage: UIImageView!
+    @IBOutlet weak var puppyImg: UIImageView!
     
     var topSegmentIndex: Int = 0
     var bottomSegmentIndex: Int = 0
@@ -23,15 +16,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         topLabel.text = "여섯가지 버튼중"
-        puppyImage.image = UIImage(named: "main")
         bottomLabel.text = "위 아래 한개씩 골라보세요!!!"
-        segmentTop.layer.zPosition = 999
-        segmentBottom.layer.zPosition = 998
-        // Do any additional setup after loading the view.
+        puppyImg.image = UIImage(named: "main")
     }
 
-    @IBAction func segmentTopChange(_ sender: Any) {
-        topSegmentIndex = segmentTop.selectedSegmentIndex
+    @IBAction func topSegmentChange(_ sender: Any) {
+        topSegmentIndex = topSegment.selectedSegmentIndex
+        refresh()
+    }
+    
+    @IBAction func bottomSegmentChange(_ sender: Any) {
+        bottomSegmentIndex = bottomSegment.selectedSegmentIndex
+        refresh()
+    }
+    
+    func refresh () {
         if topSegmentIndex == 0 {
             topLabel.text = "꽃보다 복숭복숭"
         } else if topSegmentIndex == 1 {
@@ -39,11 +38,7 @@ class ViewController: UIViewController {
         } else {
             topLabel.text = "다 내꺼 먹지마 복숭"
         }
-        print("LOG >>> \(topSegmentIndex), \(bottomSegmentIndex)")
-        puppyImage.image = UIImage(named: imageList[topSegmentIndex][bottomSegmentIndex])
-    }
-    @IBAction func segmentBottomChange(_ sender: Any) {
-        bottomSegmentIndex = segmentBottom.selectedSegmentIndex
+        
         if bottomSegmentIndex == 0 {
             bottomLabel.text = "바복숭"
         } else if bottomSegmentIndex == 1 {
@@ -51,8 +46,9 @@ class ViewController: UIViewController {
         } else {
             bottomLabel.text = "잠잔다복숭"
         }
+        
         print("LOG >>> \(topSegmentIndex), \(bottomSegmentIndex)")
-        puppyImage.image = UIImage(named: imageList[topSegmentIndex][bottomSegmentIndex])
+        puppyImg.image = UIImage(named: imageList[topSegmentIndex][bottomSegmentIndex])
     }
 }
 
